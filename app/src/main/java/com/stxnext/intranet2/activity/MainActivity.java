4405c87888658aed6ce66d63393e65592396afd0
Tomek Konieczny <tommykonieczny@gmail.com>
2015-04-22 14:40:04 +0200

    Added middleware structure

---
+++ app/src/main/java/com/stxnext/intranet2/activity/MainActivity.java
@@ -0,0 +1,9 @@
+package com.stxnext.intranet2.activity;
+
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by Tomasz Konieczny on 2015-04-22.
+ */
+public class MainActivity extends ActionBarActivity {
+}
---
+++ intranetmiddleware/src/main/java/com/stxnext/intranet2/middle/model/User.java
@@ -0,0 +1,28 @@
+package com.stxnext.intranet2.middle.model;
+
+/**
+ * Created by Tomasz Konieczny on 2015-04-22.
+ */
+public interface User {
+
+    String getFirstName();
+
+    String getLastName();
+
+    String getSkype();
+
+    String getPhoneNumber();
+
+    String getLocalization();
+
+    String getRole();
+
+    String getEmail();
+
+    String getIrc();
+
+    String getTeam();
+
+    String getPhoto();
+
+}
