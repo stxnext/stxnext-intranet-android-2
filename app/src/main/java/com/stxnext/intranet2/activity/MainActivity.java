f8a382206d7a494131d32cce943ab33389d01b8e
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-06 11:10:07 +0200

    Removed splash screen

--- app/src/main/java/com/stxnext/intranet2/activity/MainActivity.java
+++
@@ -1,9 +0,0 @@
-package com.stxnext.intranet2.activity;
-
-import android.support.v7.app.ActionBarActivity;
-
-/**
- * Created by Tomasz Konieczny on 2015-04-22.
- */
-public class MainActivity extends ActionBarActivity {
-}
---
+++ app/src/main/java/com/stxnext/intranet2/activity/ProfileActivity.java
@@ -0,0 +1,18 @@
+package com.stxnext.intranet2.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+import com.stxnext.intranet2.R;
+
+/**
+ * Created by Tomasz Konieczny on 2015-04-22.
+ */
+public class ProfileActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivty_profile);
+    }
+}
