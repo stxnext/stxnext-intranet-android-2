66c29d206bb853edb6ac9508421e1a1860369149
Łukasz Ciupa <losogitos@gmail.com>
2015-05-07 15:37:00 +0200

    New Login Activity, Flow between LoginAcitivity

--- app/src/main/java/com/stxnext/intranet2/activity/ProfileActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/ProfileActivity.java
@@ -1,7 +1,9 @@
 package com.stxnext.intranet2.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 
 import com.stxnext.intranet2.R;
 
@@ -10,9 +12,44 @@ import com.stxnext.intranet2.R;
  */
 public class ProfileActivity extends ActionBarActivity {
 
+    private static final String TAG = "ProfileActivity";
+    private static final int LOGIN_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivty_profile);
+        Intent loginIntent = new Intent(this, LoginActivity.class);
+        if (isLogged()) {
+            loadProfile();
+        } else {
+            startActivityForResult(loginIntent, LOGIN_REQUEST);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case LOGIN_REQUEST:
+                if (resultCode == LoginActivity.LOGIN_OK) {
+                    loadProfile();
+                } else {
+
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    //TODO
+    private void loadProfile() {
+        Log.d(TAG, "loadProfile()");
+    }
+    //TODO
+    private boolean isLogged() {
+        return false;
     }
 }
