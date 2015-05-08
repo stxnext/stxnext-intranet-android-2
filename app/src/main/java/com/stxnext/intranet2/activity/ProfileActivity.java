5907d32148846b1461edca2494a0470f7689992a
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-08 14:44:47 +0200

    Floating menu added

--- app/src/main/java/com/stxnext/intranet2/activity/ProfileActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/ProfileActivity.java
@@ -1,22 +1,29 @@
 package com.stxnext.intranet2.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.stxnext.intranet2.R;
 import com.stxnext.intranet2.backend.api.UserApi;
 import com.stxnext.intranet2.backend.api.UserApiImpl;
 import com.stxnext.intranet2.backend.callback.UserApiCallback;
 import com.stxnext.intranet2.backend.model.User;
+import com.stxnext.intranet2.fragment.FloatingMenuFragment;
 
 
 /**
  * Created by Tomasz Konieczny on 2015-04-22.
  */
-public class ProfileActivity extends AppCompatActivity implements UserApiCallback {
+public class ProfileActivity extends AppCompatActivity
+        implements UserApiCallback, FloatingMenuFragment.OnFlotingMenuItemClickListener {
 
     private static final String TAG = "ProfileActivity";
     private static final int LOGIN_REQUEST = 1;
@@ -26,7 +33,9 @@ public class ProfileActivity extends AppCompatActivity implements UserApiCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivty_profile);
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
-        
+
+        prepareFloatinButton();
+
         UserApi userApi = new UserApiImpl(this);
         userApi.requestForUser(null);
 
@@ -37,6 +46,37 @@ public class ProfileActivity extends AppCompatActivity implements UserApiCallbac
             startActivityForResult(loginIntent, LOGIN_REQUEST);
         }
 
+    }
+
+    private void prepareFloatinButton() {
+        ViewGroup viewGroup = (ViewGroup) findViewById(R.id.floating_button);
+        final View plusView = viewGroup.getChildAt(0);
+        if (plusView != null) {
+            viewGroup.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    plusView.animate().rotationBy(135).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                        @Override
+                        public void onAnimationEnd(Animator animation) {
+                            Fragment fragment = getFragmentManager().findFragmentByTag("Asdasd");
+
+                            if (fragment == null || !fragment.isAdded()) {
+                                getFragmentManager()
+                                        .beginTransaction()
+                                        .replace(R.id.floating_menu_container, new FloatingMenuFragment(), "Asdasd")
+                                        .setCustomAnimations(
+                                                android.R.animator.fade_in, android.R.animator.fade_out,
+                                                android.R.animator.fade_in, android.R.animator.fade_out)
+                                        .addToBackStack(null)
+                                        .commit();
+                            } else {
+                                getFragmentManager().popBackStackImmediate();
+                            }
+                        }
+                    });
+                }
+            });
+        }
     }
 
     @Override
@@ -67,6 +107,11 @@ public class ProfileActivity extends AppCompatActivity implements UserApiCallbac
 
     @Override
     public void onUserReceived(User user) {
+
+    }
+
+    @Override
+    public void onFloatingMenuItemClick(int option) {
 
     }
 }
