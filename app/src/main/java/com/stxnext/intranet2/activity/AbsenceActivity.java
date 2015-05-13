c287f2eb002fca8bb17a28369ded6882812990a9
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-13 13:57:54 +0200

    Tabs refactor in absences

--- app/src/main/java/com/stxnext/intranet2/activity/AbsenceActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/AbsenceActivity.java
@@ -1,28 +1,30 @@
 package com.stxnext.intranet2.activity;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
 import com.stxnext.intranet2.R;
 import com.stxnext.intranet2.adapter.FragmentAdapter;
 import com.stxnext.intranet2.fragment.HolidayFragment;
 import com.stxnext.intranet2.fragment.OutOfOfficeFragment;
 import com.stxnext.intranet2.fragment.WorkFromHomeFragment;
-import com.stxnext.intranet2.view.SlidingTabLayout;
 
 import java.util.ArrayList;
 
 public class AbsenceActivity extends AppCompatActivity {
 
     private ViewPager viewPager;
-    private SlidingTabLayout slidingTabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_absence);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         ArrayList<Fragment> viewPagerFragments = new ArrayList<Fragment>();
@@ -32,7 +34,8 @@ public class AbsenceActivity extends AppCompatActivity {
         FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), viewPagerFragments);
         viewPager.setAdapter(fragmentAdapter);
 
-        slidingTabLayout = (SlidingTabLayout) findViewById(R.id.sliding_tabs);
-        slidingTabLayout.setViewPager(viewPager);
+        PagerTabStrip tabStrip = (PagerTabStrip) findViewById(R.id.sliding_tabs);
+        tabStrip.setTabIndicatorColor(getResources().getColor(R.color.stxnext_green));
+        tabStrip.setTextColor(getResources().getColor(R.color.stxnext_green_dark));
     }
 }
