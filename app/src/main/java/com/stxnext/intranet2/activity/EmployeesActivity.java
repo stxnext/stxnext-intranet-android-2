a3f3ec7525e697438292c28420df303e09d9e1c9
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-14 09:38:54 +0200

    Added rounded image view

--- app/src/main/java/com/stxnext/intranet2/activity/EmployeesActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/EmployeesActivity.java
@@ -4,21 +4,38 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.stxnext.intranet2.R;
 import com.stxnext.intranet2.adapter.EmployeesAdapter;
 
 public class EmployeesActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employees);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         RecyclerView recycleView = (RecyclerView) findViewById(R.id.recycler_view);
         recycleView.setHasFixedSize(true);
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
         recycleView.setLayoutManager(layoutManager);
         EmployeesAdapter employeesAdapter = new EmployeesAdapter();
         recycleView.setAdapter(employeesAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return false;
     }
 }
