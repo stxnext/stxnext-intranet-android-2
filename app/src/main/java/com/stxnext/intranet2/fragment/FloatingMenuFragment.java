d9e1b7a71c044a05f8666959dc878e9413511a5c
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-22 14:24:10 +0200

    Floating menu animations

--- app/src/main/java/com/stxnext/intranet2/fragment/FloatingMenuFragment.java
+++ app/src/main/java/com/stxnext/intranet2/fragment/FloatingMenuFragment.java
@@ -8,6 +8,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnticipateOvershootInterpolator;
+import android.view.animation.BounceInterpolator;
+import android.view.animation.DecelerateInterpolator;
+import android.view.animation.OvershootInterpolator;
 
 import com.stxnext.intranet2.R;
 
@@ -17,7 +21,21 @@ public class FloatingMenuFragment extends Fragment {
     public static final int HOLIDAY = 1;
     public static final int OUT_OF_OFFICE = 2;
 
+    private static final String VIEW_TRANSITION_ARG = "view_transition_arg";
+
+    private int contentTransition;
     private OnFloatingMenuItemClickListener mListener;
+
+    public static FloatingMenuFragment newInstance(int floatingButtonTransition) {
+        FloatingMenuFragment fragment = new FloatingMenuFragment();
+
+        Bundle bundle = new Bundle();
+        bundle.putInt(VIEW_TRANSITION_ARG, floatingButtonTransition);
+
+        fragment.setArguments(bundle);
+
+        return fragment;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,12 +50,29 @@ public class FloatingMenuFragment extends Fragment {
         } catch (ClassCastException e) {
             throw new ClassCastException(activity.toString() + " must implement OnFloatingMenuItemClickListener");
         }
+
+        if (getArguments() != null) {
+            contentTransition = getArguments().getInt(VIEW_TRANSITION_ARG);
+        }
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.floating_view_container).animate().alpha(1f).setDuration(300);
+        view.findViewById(R.id.floating_view_container)
+                .animate()
+                .alpha(1f)
+                .setDuration(300);
+
+        ViewGroup buttons = (ViewGroup) view.findViewById(R.id.floating_buttons_container);
+        for (int i = 0; i < buttons.getChildCount(); i++) {
+            buttons.getChildAt(i).animate()
+                    .setDuration(400)
+                    .setStartDelay(100 * i)
+                    .alpha(1f)
+                    .translationY(contentTransition)
+                    .setInterpolator(new OvershootInterpolator());
+        }
     }
 
     @Override
@@ -54,10 +89,21 @@ public class FloatingMenuFragment extends Fragment {
     public void close() {
         mListener.onFloatingMenuClose();
         if (getView() != null) {
+            ViewGroup buttons = (ViewGroup) getView().findViewById(R.id.floating_buttons_container);
+            for (int i = 0; i < buttons.getChildCount(); i++) {
+                buttons.getChildAt(i).animate()
+                        .setDuration(300)
+                        .setStartDelay(100 * i)
+                        .alpha(0f)
+                        .translationYBy(-contentTransition / 4)
+                        .setInterpolator(new DecelerateInterpolator());
+            }
+
             getView().findViewById(R.id.floating_view_container)
                     .animate()
                     .alpha(0f)
                     .setDuration(300)
+                    .setStartDelay(100)
                     .setListener(new AnimatorListenerAdapter() {
                         @Override
                         public void onAnimationEnd(Animator animation) {
