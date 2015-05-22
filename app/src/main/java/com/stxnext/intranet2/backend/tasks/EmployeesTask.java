2843a3596615d0ed372d5d4403d4302999f3c1b6
losogitos <losogitos@gmail.com>
2015-05-22 17:10:48 +0200

    Merge, session save, token.

---
+++ app/src/main/java/com/stxnext/intranet2/backend/tasks/EmployeesTask.java
@@ -0,0 +1,16 @@
+package com.stxnext.intranet2.backend.tasks;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Lukasz Ciupa on 2015-05-21.
+ */
+public class EmployeesTask extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... params) {
+
+        return null;
+    }
+
+}
