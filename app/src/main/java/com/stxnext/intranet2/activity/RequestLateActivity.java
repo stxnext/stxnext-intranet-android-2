3877bba7deae94d98f83163a68d49a78aeb3041a
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-27 13:13:11 +0200

    Added new screens

---
+++ app/src/main/java/com/stxnext/intranet2/activity/RequestLateActivity.java
@@ -0,0 +1,52 @@
+package com.stxnext.intranet2.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TimePicker;
+
+import com.stxnext.intranet2.R;
+
+/**
+ * Created by Tomasz Konieczny on 2015-05-27.
+ */
+public class RequestLateActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absence_request);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        TimePicker timePicker = (TimePicker) findViewById(R.id.time_picker);
+        timePicker.setIs24HourView(true);
+
+        findViewById(R.id.submit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return false;
+    }
+
+}
