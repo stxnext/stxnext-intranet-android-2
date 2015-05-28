854af0ec7523961de7d459fa14a5b561dc08bfec
Tomek Konieczny <tommykonieczny@gmail.com>
2015-05-28 13:35:46 +0200

    Late activity UX

--- app/src/main/java/com/stxnext/intranet2/activity/RequestLateActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/RequestLateActivity.java
@@ -1,12 +1,16 @@
 package com.stxnext.intranet2.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.stxnext.intranet2.R;
@@ -36,7 +40,71 @@ public class RequestLateActivity extends AppCompatActivity {
             }
         });
 
-        View wtfView = findViewById(R.id.wtf_image);
+        final TextView hourLabel = (TextView) findViewById(R.id.hour_label);
+        final View labelBackground = findViewById(R.id.hour_label_background);
+        final View wtfView = findViewById(R.id.wtf_image);
+        final SeekBar properSeekBar = (SeekBar) findViewById(R.id.time_seek_bar);
+
+        SeekBar seekBarStub = (SeekBar) findViewById(R.id.time_seek_bar_stub);
+        seekBarStub.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            private boolean hideTip = true;
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser) {
+                    if (hideTip) {
+                        hideTip = false;
+                        final View tipView = findViewById(R.id.tip_container);
+                        tipView.animate().alpha(0).scaleX(1.5f).scaleY(1.5f).translationX(300).setDuration(300);
+                    }
+
+                    float percentage = ((float) progress / seekBar.getMax());
+                    properSeekBar.setProgress(progress);
+                    wtfView.setAlpha(percentage / 2);
+                    labelBackground.setAlpha(percentage);
+                    hourLabel.setText(convertIntToTime(progress));
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress = seekBar.getProgress();
+                int mod = progress % 5;
+                if (mod > 0) {
+                    if (mod >= 3) {
+                        int diff = 5 - mod;
+                        progress += diff;
+                    } else {
+                        progress -= mod;
+                    }
+
+                    onProgressChanged(seekBar, progress, true);
+                }
+            }
+        });
+    }
+
+    private String convertIntToTime(int progress) {
+        int hour = 9;
+
+        while (progress >= 60) {
+            hour++;
+            progress -= 60;
+        }
+
+        String minutes = String.valueOf(progress);
+        String hours = String.valueOf(hour);
+
+        if (minutes.length() == 1) {
+            minutes = "0" + minutes;
+        }
+
+        return String.format("%s:%s", hours, minutes);
     }
 
     @Override
