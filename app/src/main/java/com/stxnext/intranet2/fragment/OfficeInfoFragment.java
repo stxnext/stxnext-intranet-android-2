e9fd9bb103c4029438b41da282b9b6404eb62d92
Tomek Konieczny <tommykonieczny@gmail.com>
2015-06-12 12:57:41 +0200

    Added offices contact buttonds

--- app/src/main/java/com/stxnext/intranet2/fragment/OfficeInfoFragment.java
+++ app/src/main/java/com/stxnext/intranet2/fragment/OfficeInfoFragment.java
@@ -2,18 +2,11 @@ package com.stxnext.intranet2.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMapOptions;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.stxnext.intranet2.R;
 import com.stxnext.intranet2.model.Office;
 
@@ -23,7 +16,6 @@ import com.stxnext.intranet2.model.Office;
 public class OfficeInfoFragment extends Fragment {
 
     private static final String ARG_OFFICE = "office";
-    private static final String TAG_MAP = "map";
 
     public static OfficeInfoFragment newInstance(Office office) {
         OfficeInfoFragment fragment = new OfficeInfoFragment();
@@ -53,45 +45,16 @@ public class OfficeInfoFragment extends Fragment {
                 TextView telTextView = (TextView) view.findViewById(R.id.tel_text_view);
                 telTextView.setText(office.getTel());
 
-                TextView faxTextView = (TextView) view.findViewById(R.id.fax_text_view);
                 if (office.getFax().length() > 0) {
+                    TextView faxTextView = (TextView) view.findViewById(R.id.fax_text_view);
                     faxTextView.setText(office.getFax());
                 } else {
-                    faxTextView.setVisibility(View.GONE);
+                    view.findViewById(R.id.fax_container).setVisibility(View.GONE);
                 }
             }
-
-            prepareMap(office);
         }
 
         return view;
     }
 
-    private void prepareMap(final Office office) {
-        FragmentManager fm = getChildFragmentManager();
-        SupportMapFragment fragment = (SupportMapFragment) fm.findFragmentByTag(TAG_MAP);
-        if (fragment == null) {
-            GoogleMapOptions options = new GoogleMapOptions();
-            options.liteMode(true);
-            fragment = SupportMapFragment.newInstance(options);
-            final SupportMapFragment finalFragment = fragment;
-            getChildFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
-                @Override
-                public void onBackStackChanged() {
-                    GoogleMap map = finalFragment.getMap();
-                    MarkerOptions options = new MarkerOptions();
-                    LatLng position = new LatLng(office.getLat(), office.getLon());
-                    LatLng cameraPosition = new LatLng(office.getLat() + 0.02, office.getLon());
-                    options.position(position);
-                    map.moveCamera(CameraUpdateFactory.newLatLngZoom(cameraPosition, 12));
-                    map.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
-                    map.addMarker(options);
-                    map.getUiSettings().setAllGesturesEnabled(false);
-                }
-            });
-
-            fm.beginTransaction().replace(R.id.map_container, fragment, TAG_MAP).addToBackStack(null).commit();
-        }
-
-    }
 }
