655101de730c9aca94d709d29a5bee8d6d743316
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2015-08-20 11:43:36 +0200

    refactoring: downloadUsers, downloadUser

--- app/src/main/java/com/stxnext/intranet2/utils/DBManager.java
+++ app/src/main/java/com/stxnext/intranet2/utils/DBManager.java
@@ -2,7 +2,9 @@ package com.stxnext.intranet2.utils;
 
 import android.content.Context;
 
+import com.google.common.base.Optional;
 import com.stxnext.intranet2.backend.api.EmployeesCommonApi;
+import com.stxnext.intranet2.backend.callback.EmployeesApiCallback;
 import com.stxnext.intranet2.backend.model.impl.User;
 import com.stxnext.intranet2.database.DatabaseHelper;
 import com.stxnext.intranet2.database.repo.UserRepository;
@@ -25,7 +27,7 @@ public class DBManager {
 
             //isLoaded is false on app start - contacts are refreshed
             if (!isLoaded)
-                EmployeesCommonApi.downlUsersFromHTTP(context, null, null);
+                EmployeesCommonApi.downloadUsers(context, Optional.<EmployeesApiCallback>absent());
         }
         return instance;
     }
