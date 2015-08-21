8c20f3ce05ddacdd2492cdf87cd18f11549f6551
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2015-08-21 10:50:04 +0200

    retrofit: get worked hours for user stub

---
+++ app/src/main/java/com/stxnext/intranet2/backend/retrofit/WorkedHoursService.java
@@ -0,0 +1,18 @@
+package com.stxnext.intranet2.backend.retrofit;
+
+import com.stxnext.intranet2.backend.model.workedHour.WorkedHours;
+
+import java.util.List;
+
+import retrofit.http.GET;
+import retrofit.http.Path;
+import retrofit.http.Query;
+
+/**
+ * Created by bkosarzycki on 21.08.15.
+ */
+public interface WorkedHoursService {
+
+    @GET("/api/worked_hours")
+    List<WorkedHours> getUserWorkedHours(@Query("user_id") int user);
+}
