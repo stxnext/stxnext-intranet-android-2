aa2692cc7e52f42ae3cc3ff867076f153b3365f6
losogitos <losogitos@gmail.com>
2015-08-21 11:29:05 +0200

    Profile animation, profile image clickable.

--- app/src/main/java/com/stxnext/intranet2/view/ProfileView.java
+++ app/src/main/java/com/stxnext/intranet2/view/ProfileView.java
@@ -19,6 +19,7 @@ public class ProfileView extends FrameLayout {
     private ImageView imageViewStandard;
     private FrameLayout standardProfileHeaderCointainer;
     private FrameLayout superHeroProfileHeaderContainer;
+    private ImageView scrollViewHeaderImageViewSpace;
 
     public ProfileView(Context context) {
         super(context);
@@ -40,6 +41,7 @@ public class ProfileView extends FrameLayout {
         imageViewStandard = (ImageView) findViewById(R.id.profile_image_view_standard);
         standardProfileHeaderCointainer = (FrameLayout) findViewById(R.id.standard_profile_header_container);
         superHeroProfileHeaderContainer = (FrameLayout) findViewById(R.id.superhero_profile_header_container);
+        scrollViewHeaderImageViewSpace = (ImageView) findViewById(R.id.scroll_view_profile_image_view_mapper);
         if (imageView == null) {
             throw new RuntimeException("ImageView with id profile_image_view not found in ProfileView");
         }
@@ -62,6 +64,8 @@ public class ProfileView extends FrameLayout {
                 int translationY = scrollView.getScrollY();
 //                translateImageViews(translationY);
                 translateHeaders(translationY);
+                translateUpperLayerProfileImageOnClickMapper(translationY);
+
                 toolbar.setTranslationY(-translationY / 4);
 
                 if (floatingButton != null) {
@@ -73,6 +77,21 @@ public class ProfileView extends FrameLayout {
             }
         });
 
+    }
+
+    /**
+     * There is a circle image view mapper in upper layer in this view which executes onClicks.
+     * This is a transparent circle view which is in the same place as below image view.
+     * It's because it's was difficult to pass onClick event from this upper layer to imageView below.
+     * Because it is scrolled with scroll view (to wchich it belongs) there is need to translate it
+     * back to position of below image view .They are translated one unit for every 4 for scroll view
+     * so they are translated by 1/4 (look at translateHeaders()) so there is need to back it by 3/4
+     * to be on the righ place.
+     *
+     * @param translationY
+     */
+    private void translateUpperLayerProfileImageOnClickMapper(int translationY) {
+        scrollViewHeaderImageViewSpace.setTranslationY(3 * (translationY / 4));
     }
 
     private void translateImageViews(int translationY) {
