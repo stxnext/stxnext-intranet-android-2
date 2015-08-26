0f8107c52cb88a0a56420d58c8408b75acafca48
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2015-08-26 15:17:02 +0200

    stetho only in debug

--- app/src/main/java/com/stxnext/intranet2/IntranetApp.java
+++ app/src/main/java/com/stxnext/intranet2/IntranetApp.java
@@ -2,6 +2,8 @@ package com.stxnext.intranet2;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
+
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp.StethoInterceptor;
 import com.squareup.okhttp.OkHttpClient;
@@ -15,17 +17,18 @@ public class IntranetApp extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Context context = getApplicationContext();
+        boolean isDebuggable =  ( 0 != ( getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE ) );
+        if (BuildConfig.DEBUG && isDebuggable) {
+            Stetho.initialize(
+                    Stetho.newInitializerBuilder(this)
+                            .enableDumpapp(
+                                    Stetho.defaultDumperPluginsProvider(this))
+                            .enableWebKitInspector(
+                                    Stetho.defaultInspectorModulesProvider(this))
+                            .build());
 
-        Stetho.initialize(
-                Stetho.newInitializerBuilder(this)
-                        .enableDumpapp(
-                                Stetho.defaultDumperPluginsProvider(this))
-                        .enableWebKitInspector(
-                                Stetho.defaultInspectorModulesProvider(this))
-                        .build());
-
-        OkHttpClient client = new OkHttpClient();
-        client.networkInterceptors().add(new StethoInterceptor());
+            OkHttpClient client = new OkHttpClient();
+            client.networkInterceptors().add(new StethoInterceptor());
+        }
     }
 }
