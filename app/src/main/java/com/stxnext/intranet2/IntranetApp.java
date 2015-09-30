a7931a8d15c4db01f9b4f930fc298dc5a738e601
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2015-09-30 12:33:34 +0200

    logout on creation

--- app/src/main/java/com/stxnext/intranet2/IntranetApp.java
+++ app/src/main/java/com/stxnext/intranet2/IntranetApp.java
@@ -7,15 +7,24 @@ import android.content.pm.ApplicationInfo;
 import com.facebook.stetho.Stetho;
 import com.facebook.stetho.okhttp.StethoInterceptor;
 import com.squareup.okhttp.OkHttpClient;
+import com.stxnext.intranet2.database.DatabaseHelper;
+import com.stxnext.intranet2.utils.DBManager;
 
 
 /**
  * Created by bkosarzycki on 26.08.15.
  */
 public class IntranetApp extends Application {
+
+    static Context mContext;
+
     @Override
     public void onCreate() {
         super.onCreate();
+
+        mContext = this.getApplicationContext();
+        //create database for db-versioning (on app startup)
+        DBManager.getInstance(mContext).getEmployees();
 
         boolean isDebuggable =  ( 0 != ( getApplicationInfo().flags & ApplicationInfo.FLAG_DEBUGGABLE ) );
         if (BuildConfig.DEBUG && isDebuggable) {
@@ -30,5 +39,9 @@ public class IntranetApp extends Application {
             OkHttpClient client = new OkHttpClient();
             client.networkInterceptors().add(new StethoInterceptor());
         }
+    }
+
+    public static Context getContext() {
+        return mContext;
     }
 }
