44d5eebed2af62907acd6eefac11855aec920ed4
Bartosz Kosarzycki <kosiara87@gmail.com>
2015-11-08 19:54:32 +0100

    fake ProjectListService

---
+++ app/src/main/java/com/stxnext/intranet2/backend/model/project/Project.java
@@ -0,0 +1,26 @@
+package com.stxnext.intranet2.backend.model.project;
+
+/**
+ * Created by bkosarzycki on 02.11.15.
+ */
+public class Project {
+    private int id;
+
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
