1a5b7e013781846e3ded3b8e143824e379364266
Bartosz Kosarzycki <kosiara87@gmail.com>
2015-11-18 17:12:03 +0100

    separate UserApiTimeReportCallback

--- app/src/main/java/com/stxnext/intranet2/backend/api/UserApi.java
+++ app/src/main/java/com/stxnext/intranet2/backend/api/UserApi.java
@@ -3,6 +3,7 @@ package com.stxnext.intranet2.backend.api;
 import android.content.Context;
 
 import com.stxnext.intranet2.backend.callback.UserApiCallback;
+import com.stxnext.intranet2.backend.callback.UserApiTimeReportCallback;
 import com.stxnext.intranet2.model.HolidayTypes;
 
 import java.util.Calendar;
@@ -33,6 +34,6 @@ public abstract class UserApi extends EmployeesCommonApi {
 
     public abstract void getAbsenceDaysLeft();
 
-    public abstract void getTimeReport(String userId, Calendar month);
+    public abstract void getTimeReport(String userId, Calendar month, UserApiTimeReportCallback callback);
 
 }
