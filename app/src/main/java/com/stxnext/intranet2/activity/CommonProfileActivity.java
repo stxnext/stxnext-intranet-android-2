710c6c039adafacdcfa38d620f3e18c7e9ee0c01
Bartosz Kosarzycki <kosiara87@gmail.com>
2015-11-19 14:43:25 +0100

    rotate anim

--- app/src/main/java/com/stxnext/intranet2/activity/CommonProfileActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/CommonProfileActivity.java
@@ -11,7 +11,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
 import android.view.animation.OvershootInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -94,8 +96,18 @@ public abstract class CommonProfileActivity extends AppCompatActivity implements
         downloadTodayHoursBckg(user);
 
         workedHoursRefreshHoursCardIv.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) { downloadTodayHoursBckg(user); }
+            @Override public void onClick(View v) { downloadTodayHoursBckg(user); rotateRefreshImageView(); }
         });
+    }
+
+    private void rotateRefreshImageView() {
+        RotateAnimation rotate = new RotateAnimation(0, 180,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        rotate.setDuration(600);
+        //rotate.setRepeatCount(Animation.INFINITE);
+        workedHoursRefreshHoursCardIv.startAnimation(rotate);
     }
 
     private void downloadTodayHoursBckg(final User user) {
