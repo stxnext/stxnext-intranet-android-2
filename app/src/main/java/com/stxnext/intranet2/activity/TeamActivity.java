e40d78422d056bbbd726bb16d5ac436d54474db4
Łukasz Ciupa <losogitos@gmail.com>
2015-11-25 18:00:41 +0100

    Team activity initialization.

--- app/src/main/java/com/stxnext/intranet2/activity/TeamActivity.java
+++ app/src/main/java/com/stxnext/intranet2/activity/TeamActivity.java
@@ -3,11 +3,14 @@ package com.stxnext.intranet2.activity;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.stxnext.intranet2.R;
 import com.stxnext.intranet2.adapter.EmployeesListAdapter;
+import com.stxnext.intranet2.backend.service.TeamCacheService;
 
 /**
  * Created by Łukasz Ciupa on 25.11.2015.
@@ -18,6 +21,7 @@ public class TeamActivity extends AppCompatActivity {
     private Toolbar toolbar;
     private EmployeesListAdapter adapter;
     private SwipeRefreshLayout swipeRefreshLayout;
+    private TeamCacheService teamCacheService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,29 @@ public class TeamActivity extends AppCompatActivity {
         setContentView(R.layout.activity_team);
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
+        swipeRefreshLayout.setColorSchemeResources(
+                R.color.stxnext_green_dark,
+                R.color.stxnext_green,
+                R.color.stxnext_green_darkest);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        teamCacheService = TeamCacheService.getInstance(this);
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+        return false;
     }
 }
