646f00f315ab81be834397dd3aba2cd870d28a82
Bartosz Kosarzycki <bartosz.kosarzycki@stxnext.pl>
2016-02-25 11:37:22 +0100

    new api key

--- app/src/main/java/com/stxnext/intranet2/utils/ConnectionManager.java
+++ app/src/main/java/com/stxnext/intranet2/utils/ConnectionManager.java
@@ -19,7 +19,12 @@ import java.io.IOException;
  */
 public class ConnectionManager {
 
-    private static final String LOGIN_URL = "https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&redirect_uri=https%3A%2F%2Fintranet-staging.stxnext.pl%2Fauth%2Fcallback&response_type=code&client_id=83120712902.apps.googleusercontent.com&access_type=offline";
+    private static final String LOGIN_URL = "https://accounts.google.com/o/oauth2/auth?" +
+            "scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcalendar.readonly&" +
+            "redirect_uri=https%3A%2F%2Fintranet-staging.stxnext.pl%2Fauth%2Fcallback&" +
+            "response_type=code&" +
+            "client_id=83120712902-4fud2l3ki3s995c6m8bmebabqca1do0f.apps.googleusercontent.com&" +
+            "access_type=offline";
 
     private Context context;
     private ConnectionCallback callback;
