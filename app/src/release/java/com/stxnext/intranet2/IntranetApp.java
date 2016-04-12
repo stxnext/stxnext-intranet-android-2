92bedb77b5bcd765d47e3ea71fba4c193d61c802
Mariusz Krok <mariusz.krok@stxnext.pl>
2016-04-12 15:58:53 +0200

    Travis buil fixes

---
+++ app/src/release/java/com/stxnext/intranet2/IntranetApp.java
@@ -0,0 +1,35 @@
+package com.stxnext.intranet2;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.ContextWrapper;
+
+import com.pixplicity.easyprefs.library.Prefs;
+
+
+/**
+ * Created by bkosarzycki on 26.08.15.
+ */
+public class IntranetApp extends Application {
+
+    static Context mContext;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        mContext = this.getApplicationContext();
+
+        new Prefs.Builder()
+                .setContext(this)
+                .setMode(ContextWrapper.MODE_PRIVATE)
+                .setPrefsName(getPackageName())
+                .setUseDefaultSharedPreference(true)
+                .build();
+    }
+
+    public static Context getContext() {
+        return mContext;
+    }
+}
+;
